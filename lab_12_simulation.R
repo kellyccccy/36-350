43981a88d2e19c7aadbf2fd791aaa0cacc1eb76a
kellyccccy <kellychenyi@gmail.com>
2018-04-19 18:04:19 -0400

    Task 2 (2b)

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,15 @@ generate_data = function (n, p) {
   res = rnorm(n)
   return (list(covariates = mat, responses = res))
 }
+
+model_select = function (covariates, responses, cutoff) {
+  my.lm = lm(responses ~ covariates)
+  p-values = summary(my.lm)$coefficients[,4]
+  retained = covariates[,p-values <= cutoff]
+  if (length(retained) == 0) return (vector(length = 0))
+  else {
+    new.lm = lm(responses ~ retained)
+    new.p-values = summary(new.lm)$coefficients[,4]
+    return (new.p-values)
+  }
+}
